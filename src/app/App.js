a593f5d73c02f3008d16360e96e9ee76db6de9a1
gabderrico05 <derrico.g@aluno.ifsp.edu.br>
2025-08-26 18:57:20 -0300

    NativeWind configurado

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import "./global.css"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View className="flex-1 items-center justify-center bg-white">
+    <Text className="text-xl font-bold text-blue-500">
+      Welcome to Nativewind!
+    </Text>
+  </View>
   );
 }
 
